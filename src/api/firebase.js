196a0bdd2d86a0dbd3dfab3d9f526730221335b8
Muhammed Ziyad <muhdziyad@gmail.com>
2023-09-08 18:12:39 +0530

    some env editing

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -4,12 +4,12 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASEAPI,
-  authDomain: "fir-2092e.firebaseapp.com",
-  projectId: "fir-2092e",
-  storageBucket: "fir-2092e.appspot.com",
-  messagingSenderId: "761309781602",
-  appId: "1:761309781602:web:547726b49c5305e51a3b3b",
-  measurementId: "G-101XYR1H6E"
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId:import.meta.env.VITE_PROJECTID  ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APIID,
+  measurementId:import.meta.env.VITE_MEASUREMENTID
 };
 
 export const app = initializeApp(firebaseConfig);
