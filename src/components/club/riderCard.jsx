cde1fbf1b200273853bb511c85905b5fbd113c52
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-10-01 15:49:37 +0530

    some ui edits in the rider component

--- src/components/club/riderCard.jsx
+++ src/components/club/riderCard.jsx
@@ -7,15 +7,15 @@ function RiderCard(props) {
   const userId = props.userId
   return (
     <div className='bg-slate-700 rounded-sm p-2 capitalize'>
-      <div className='relative  flex justify-center'>
-        <img className='h-40 sm:h-60 md:h-72 w-3/4 sm:w-full' src={rider.rider?.profileImage ? rider.rider?.profileImage : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'} alt="" />
-        <div className='bg-blue-500 absolute -bottom-1 h-6 w-3/4'>
-          <p className='text-white text-center'>{rider.rider.name}</p>
+      <div className='relative flex justify-center'>
+        <img className='h-40 rounded-sm sm:h-60 md:h-72 w-3/4 sm:w-full' src={rider.rider?.profileImage ? rider.rider?.profileImage : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'} alt="" />
+        <div className='bg-blue-500 rounded-sm absolute -bottom-1 h-6 w-3/4'>
+          <p className='text-white text-center' >{rider.rider.name}</p>
         </div>
       </div>
-      <p className='text-slate-400 text-center mt-1 overflow-hidden'>Phone : {rider.rider.phone}</p>
+      <a className='text-slate-400 text-center mt-1 overflow-hidden' href={`tel:${rider?.rider?.phone}`}> Phone : {rider?.rider?.phone||'Not Provided'}</a>
       <div className='flex justify-between p-2'>
-        {ride?.head._id === userId ? <button onClick={() => removeRider(ride._id, rider.rider._id)} className='bg-red-600 rounded-md text-white px-1'>Remove</button> : ''}
+        {ride?.head._id === userId ? <p onClick={() => removeRider(ride._id, rider.rider._id)} className=' text-red-400 hover:text-red-600 cursor-pointer px-1'>Remove</p> : ''}
       </div>
     </div>
   )
